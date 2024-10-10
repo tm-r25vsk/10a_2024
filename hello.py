ec93697ae0c2e82b77b9bbe4d8e4ba8d95891857
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2024-10-10 09:30:29 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Sveika, skola!!")
+vards = input("Kā tevi sauc? ")
+# skola = "Rīgas 25.vidusskola"
+# Skola = "6.vidusskola"
+# print(Skola)
+# print(skola)
+# a = 3+7
+# a = a+2
+# print(a)
+# b = 12
+# c = 7
+# print("suns" + "kaķis")
+# print("c" + "b")
+# print(c + b)
+print("sveiki, " + vards)
