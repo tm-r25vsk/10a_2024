ce6a12bd5adf8cc0108d520554c7e35d26b1a3a8
tm-r25vsk <r25vsk@r25vsk.edu.lv>
2024-10-17 09:26:44 +0300

    Update 03  paritāte.py

---
+++ Before/03  paritāte.py
@@ -0,0 +1,15 @@
+import os 
+os.system('cls') # attīra termināla logu pašā sākumā
+print("\n")  # ieliek tukšu rindiņu pirms izdrukas 
+
+x = int(input("Kāda ir skaitļa vērtība? "))
+if x % 2 == 0:
+    print("Pāra skaitlis")
+else:
+    print("Nepāra skaitlis")
+    
+    
+    
+    
+    
+print("\n")
