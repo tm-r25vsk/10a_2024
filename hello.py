fbae541626695bae533a98546d927dd778883fa8
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2024-12-18 17:51:54 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-def mana(a,b):
- return a+b
-x = input()
-y = input()
-z=mana(x,y)
-print(z)
+# def mana(a,b):
+#  return a+b
+# x = input()
+# y = input()
+# z=mana(x,y)
+# print(z)
+
+import requests
+
+x = requests.get('https://w3schools.com/python/demopage.htm')
+
+print(x.text)
